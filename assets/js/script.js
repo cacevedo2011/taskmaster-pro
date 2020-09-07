22cc44b51be7a4e29a731728d17efcb31cd6c507
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-09-07 14:48:03 -0400

    finished the drag and drop feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -150,19 +150,19 @@ $(".list-group").on("click", "span", function() {
 $(".list-group").on("blur", "input[type='text']", function() {
   // get current text
   var date = $(this)
-    .val()
-    .trim();
+      .val()
+      .trim();
 
   // get the parent ul's id attribute
   var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+      .closest(".list-group")
+      .attr("id")
+      .replace("list-", "");
 
   // get the task's position in the list of other li elements
   var index = $(this)
-    .closest(".list-group-item")
-    .index();
+      .closest(".list-group-item")
+      .index();
 
   // update task in array and re-save to localstorage
   tasks[status][index].date = date;
@@ -170,22 +170,91 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // recreate span element with bootstrap classes
   var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(date);
+      .addClass("badge badge-primary badge-pill")
+      .text(date);
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
 });
 
-
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
-    tasks[key].length = 0;
-    $("#list-" + key).empty();
+      tasks[key].length = 0;
+      $("#list-" + key).empty();
   }
   saveTasks();
 });
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+
+  },
+  deactivate: function(event) {
+
+  },
+  over: function(event) {
+
+  },
+  out: function(event) {
+
+
+  },
+  // loop over current set of children in sortable lists
+  update: function(event) {
+
+      // array to store the task data in 
+      var tempArr = [];
+
+      $(this).children().each(function() {
+          var text = $(this)
+              .find("p")
+              .text()
+              .trim();
+
+          var date = $(this)
+              .find("span")
+              .text()
+              .trim();
+
+          // add task data to the temp array as an object 
+          tempArr.push({
+              text: text,
+              date: date
+          });
+      });
+
+      // trim down list's ID to match object property
+      var arrName = $(this)
+          .attr("id")
+          .replace("list-", "");
+
+      // update an array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+      console.log(tempArr);
+  }
+
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+      ui.draggable.remove();
+      console.log("drop");
+  },
+  over: function(event, ui) {
+      console.log("over");
+  },
+  out: function(event, ui) {
+      console.log("out");
+  }
+})
 
 // load tasks for the first time
 loadTasks();
